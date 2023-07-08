f4ac20c0fab5fc25027d4add8a0ccd44609f6771
Marisi Romanillos <marisiromanillos@Marisis-MacBook-Pro.local>
2023-07-08 13:03:11 +0100

    added animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Counter from "./components/Counter";
+import Animation from "./components/Animation";
 
 const App = () => {
   const birthdate = "1988-02-02"; // Replace with the actual birthdate
 
   return (
     <div>
+      <h1>Welcome LoveBird</h1>
       <h1>Welcome LoveBird</h1>
     </div>
   );
