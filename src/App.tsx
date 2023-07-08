e13ae04fe782ada5bfeb4c34b3cb9819880e5fef
luca98-developer <lucacaimi@yahoo.com>
2023-07-08 11:37:22 +0100

    q

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Interface from "./Interface";
 
 function App() {
   return (
     <>
       <div className="p-0 m-0">
         <p>Stuff goes here</p>
+        <Interface />
       </div>
     </>
   );
