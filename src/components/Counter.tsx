4dc7a1c1507a930dc4c47c94c26831283eb2b52f
Marisi Romanillos <marisiromanillos@Marisis-MacBook-Pro.local>
2023-07-08 11:57:12 +0100

    fix error counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 
 const Counter: React.FC<{ birthdate: string }> = ({ birthdate }) => {
-  const [daysLeft, setDaysLeft] = useState<number>(calculateDaysLeft());
-
   const calculateDaysLeft = (): number => {
     const today = new Date();
     const birthdateDate = new Date(birthdate);
     birthdateDate.setFullYear(today.getFullYear());
 
+    // Adjust the birthdate to the next year if it has already passed
     if (today > birthdateDate) {
       birthdateDate.setFullYear(today.getFullYear() + 1);
     }
 
+    // Calculate the difference in milliseconds between the birthdate and today
     const timeDiff = birthdateDate.getTime() - today.getTime();
+
+    // Convert milliseconds to days
     const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
     return days;
   };
+
+  const [daysLeft, setDaysLeft] = useState<number>(calculateDaysLeft());
 
   return (
     <div>
